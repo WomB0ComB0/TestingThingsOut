a77e9070accafa82d2f03c1b6dacc46e916667b1
Mike Odnis <airwolf635@gmail.com>
2023-05-20 14:26:22 -0400

    Daily commit, may not be much, but its honest work.

---
+++ Practice/JavaScript/Pet Code/RmExtraSpacesFromString.js
@@ -0,0 +1,16 @@
+let str = ' My     name     is    Mike'
+const space = str.replace(/\s+/g,' ').trim(); // using regular expression to replace all spaces with one space and then trim the string to remove the space at the beginning and end of the string.
+console.log(space);
+
+// let str = ' My     name     is    Mike'
+// function spaces() {
+//   let arr = str.split(' ')
+//   let add = [];
+//   arr.forEach((item) => {
+//     if (item > 0) {
+//       add.push(item)
+//     }
+//   })
+//   return add.join(' ');
+// }
+// console.log(spaces());
