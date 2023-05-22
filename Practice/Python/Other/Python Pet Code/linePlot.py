825891a60e7875afa406200f49ab59a1b27128e1
Mike Odnis <airwolf635@gmail.com>
2023-05-22 01:58:13 -0400

    Daily commit, may not be much, but its honest work.

---
+++ Practice/Python/Other/Python Pet Code/linePlot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+listOne = [1, 2, 3, 4, 5]
+listTwo = [2, 4, 6, 8, 10]
+
+plt.plot(listOne, listTwo)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Line Plot')
+
+plt.show()
+#pip install matplotlib
+#pip install numpy
