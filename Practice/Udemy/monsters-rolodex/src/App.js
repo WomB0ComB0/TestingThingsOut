db156b4d1e7ec3e251f7e98701e087f587db023c
Mike Odnis <airwolf635@gmail.com>
2023-05-28 21:25:37 -0400

    Daily commit, may not be much, but its honest work.

---
+++ Practice/Udemy/monsters-rolodex/src/App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import CardList from './components/card-list/card-list.component';
+import './App.css';
+
+class  App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState(() => { return {monsters: users}}, () => {console.log(this.state.monsters)}));
+  }
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLowerCase();
+    this.setState(() => { return { searchField } });
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+    return (
+      <div className="App">
+        <input 
+          className="search-box" 
+          type="search" 
+          placeholder="search monsters" 
+          onChange={onSearchChange} 
+        />
+        {/* {
+          filteredMonsters.map(monster => <div key={monster.id}><h1>{monster.name}</h1></div>)
+        } */}
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
