8be5f22eab5b91ea177c1c124baadb83bacc0e49
Mike Odnis <airwolf635@gmail.com>
2023-06-09 16:09:14 -0400

    Daily commit, may not be much, but its honest work.

---
+++ Practice/React/components/buttons/read-more-read-less/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReadMoreReadLess from './ReadMoreReadLess';
+export default function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <ReadMoreReadLess maxCharacterCount={155}>
+        Lorem ipsum dolor sit amet delenit lorem at lorem elit eos dolore sea sit minim. Eos delenit suscipit. Et gubergren et dolor amet lorem sed facer odio consetetur. Vero rebum sit tempor eos no takimata. Facilisi stet eu est eleifend nibh labore sit et dolore et blandit diam dolor gubergren sanctus suscipit mazim aliquyam. Vel volutpat eirmod ipsum est tempor. Diam takimata zzril velit tempor. Exerci dolores accusam eos aliquip velit invidunt dolores sea amet no dolor amet suscipit imperdiet est amet consequat. Sed sanctus invidunt accusam magna erat adipiscing duis sit aliquyam hendrerit ipsum te consequat. Nihil dolore est et lorem dolor at illum accumsan eros vero. Dolor aliquip stet et clita. Invidunt no kasd et sit sed vel et sed facer tempor amet at autem erat elitr. Dolores amet erat vel. Sit est elit stet takimata vero est clita quod luptatum tincidunt dolores stet. Dolor voluptua amet ea at et at et consectetuer tempor et. Vel sit mazim et eos praesent hendrerit diam takimata magna aliquam aliquip labore et ea ea. Esse gubergren sea. Sit justo dolore nobis invidunt accusam kasd dolore sed eos kasd. Aliquyam congue lorem lorem vero et gubergren takimata sadipscing. Duo dolore et aliquyam et justo gubergren voluptua consetetur. Sea zzril hendrerit nobis. Molestie sit dolore amet at sed. Voluptua lorem lorem et kasd. In lorem amet erat dolor lorem nobis aliquyam erat sanctus sit sadipscing te. Erat et velit ea est accusam. Consequat accusam dolore no justo in dolor. Erat voluptua ipsum sea amet cum nulla sed exerci. Sed lorem augue justo in te sanctus rebum voluptua. Sadipscing et eirmod. Facilisi enim et.
+      </ReadMoreReadLess>
+    </div>
+  );
+}
---
+++ Practice/React/components/buttons/read-more-read-less/src/ReadMoreReadLess.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+const Card = ({ children }) => {
+  return (
+    <div id='Card' style={{ borderRadius: '12px', padding: '10px', margin: '10px', paddingBottom: '2ppx', width: '600px', boxShadow: '0px 2px 5px 0px rgba(0, 0, 0, 0.5)' }}>
+      {children}
+    </div>
+  );
+};
+const ReadMoreReadLess = ({ children, maxCharacterCount = 150 }) => {
+  const [isTruncated, setIsTruncated] = useState(true);
+  const resultString = isTruncated ? children.slice(0, maxCharacterCount) + " . . .": children;
+  const date = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  return (
+    <Card>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '600px', margin: '0px', alignItems: 'flex-start' }}>
+        <span style={{display: 'flex', flexDirection: 'row'}}>
+          <h4 style={{ marginTop: '4px' ,marginRight: 'auto', marginBottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>Mike Odnis <span style={{ color: 'gray', fontSize: '12px' }}>@mikeodnis</span></h4>(he/him)
+        </span>
+          <p style={{ overflow: 'hidden', textOverflow: 'ellipsis', margin: 'auto', padding: '10px 0px', maxHeight: isTruncated ? '60px' : 'none', transition: 'max-height 0.5 ease', display: 'inline' }}>{resultString}   
+          {children.length > maxCharacterCount && (
+            <span
+              style={{ color: 'grey', cursor: 'pointer', marginTop: 'auto', marginLeft: 'auto', transition: 'color 0.3s ease', marginBottom: 0, paddingRight: '10px', fontSize: '14px', fontWeight: 'bold', textDecoration: 'underline', textDecorationColor: 'grey', textDecorationThickness: '1px', textDecorationStyle: 'dotted', paddingLeft: 'auto', position: 'relative', bottom: '0px', display: 'inline', float: 'right', userSelect: 'none' }}
+              onClick={() => setIsTruncated(!isTruncated)}
+            >
+              {isTruncated ? 'more' : 'less'}
+            </span>
+          )}        
+          </p>
+          <>
+            <span style={{ color: 'grey', fontSize: '12px', marginRight: 'auto', marginBottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', userSelect: 'none' }}>{`${date}`}</span>
+        </>
+      </div>
+    </Card>
+  );
+};
+export default ReadMoreReadLess;
