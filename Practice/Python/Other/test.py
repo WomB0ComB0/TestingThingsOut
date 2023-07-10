f87bf68d0e102903f88f2a20ae5bef8baa4ac9f5
Mike Odnis <airwolf635@gmail.com>
2023-07-09 21:54:21 -0400

    Daily commit, may not be much, but its honest work.

--- Practice/Python/Other/test.py
+++ Practice/Python/Other/test.py
@@ -88,20 +88,117 @@
         # .pop(0) removes the first element of the array and returns ite
         # array = list(zip(*array))[::-1] #  rotate the remaining array clockwise
     # return traversedArray
-scores = [12, 20, 10, 24]
-def breakingRecords(scores):
-    minMax = [0,0]
-    minScore = 0
-    maxScore = 0
-    for i in range(len(scores)):
-        if i == 0:
-            minScore = scores[i]
-            maxScore = scores[i]
-        elif scores[i] < minScore:
-            minScore = scores[i]
-            minMax[1] += 1
-        elif scores[i] > maxScore:
-            maxScore = scores[i]
-            minMax[0] += 1
-    return minMax
-print(breakingRecords(scores))
+# scores = [12, 20, 10, 24]
+# def breakingRecords(scores):
+    # minMax = [0,0]
+    # minScore, maxScore = scores[0], scores[0]
+    # for i in range(len(scores)):
+        # if scores[i] < minScore:
+            # minScore = scores[i]
+            # minMax[1] += 1
+        # elif scores[i] > maxScore:
+            # maxScore = scores[i]
+            # minMax[0] += 1
+    # return minMax
+# print(breakingRecords(scores))
+# Find individual lengths of each subarray in a 2D array
+# def findLengths(array):
+    # lengths = []
+    # for i in range(len(array)):
+        # lengths.append(len(array[i]))
+    # return lengths
+# print(findLengths([[1,2,3],[4,5],[6,7,8,9]]))
+# Find the sum of each subarray in a 2D array
+# def findSums(array):
+    # sums = []
+    # for i in range(len(array)):
+        # sums.append(sum(array[i]))
+    # return sums
+# print(findSums([[1,2,3],[4,5],[6,7,8,9]]))
+# Find the sum of each subarray in a 2D array and return the sum of the sums
+# def findSumOfSums(array):
+    # sums = []
+    # for i in range(len(array)):
+        # sums.append(sum(array[i]))
+    # return sum(sums)
+# print(findSumOfSums([[1,2,3],[4,5],[6,7,8,9]]))
+
+# data = [
+#   [23, 11, 5, 14],
+#   [8, 32, 20, 5]
+# ]
+# color = input()
+# sums = []
+# def findSumOfSums(data):
+#   for i in range(len(data)):
+    #   sums.append(sum(data[i]))
+#   return sum(sums)
+# total = findSumOfSums(data)
+# if color == "brown":
+    # print(int((data[0][0] + data[1][0])*100/total))
+# elif color == "blue":
+    # print(int((data[0][1]+data[1][1])*100/total))
+# elif color == "green":
+    # print(int((data[0][2]+data[1][2])*100/total))
+# else:
+    # print(int((data[0][3]+data[1][3])*100/total))
+
+# x=[1,2,3,4,5,6]
+# x.append(8)
+# x.insert(0, 0)
+# x.remove(2)
+# print(len(x))
+
+# prices = [125000, 78000, 110000, 65000, 300000, 250000, 210000, 150000, 165000, 140000, 125000, 85000, 90000, 128000, 230000, 225000, 100000, 300000]
+# average = int(sum(prices) / len(prices))
+# count = 0
+# for i in range(len(prices)):
+    # if prices[i] > average:
+        # count += 1
+# print(count)
+
+# insects = [n*2**i for i in range(12)]
+
+# nums = (55, 44, 33, 22)
+# print(max(min(nums[:2]), abs(-42)))
+
+# text = input()
+# print((sum(len(word) for word in text.split()))/len(text.split()))
+
+# contacts = { "David": ["123-321-88", "david@test.com"], "James": ["241-879-093", "james@test.com"], "Bob": ["987-004-322", "bob@test.com"], "Amy": ["340-999-213", "a@test.com"] }
+# name = input()
+# if name in contacts:
+    # print(contacts[name][1])
+# else:
+    # print("Not found")
+# name = input()
+# if name in contacts:
+    # print(contacts[name][1])
+# else:
+    # print("Not found")
+
+# fib = {1: 1, 2: 1, 3: 2, 4: 3}
+# print(fib.get(4, 0) + fib.get(7, 5))
+
+# distance = list(range(len(points)))
+# z = 0
+# for (x, y) in points:
+    # distance[z] = (x**2 + y**2)**0.5
+    # z += 1
+# print(min(distance))
+
+# list1 = s1.split()
+# list2 = s2.split()
+# print(len(set(list1) & set(list2)))
+# 
+# n = p.head
+# while n != None:
+    # print(n.title)
+    # n = n.next
+# a = 0
+# for i in range(len(x.adj)):
+    # if i+1==n:
+        # for j in x.adj[i]:
+            # if j==1:
+                # a+=1
+# print(a)
