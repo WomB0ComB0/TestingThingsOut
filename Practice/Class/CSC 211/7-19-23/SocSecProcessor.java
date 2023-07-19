f1f7fbf55a751f5f6ce5676f916c746afc897bce
Mike Odnis <airwolf635@gmail.com>
2023-07-19 13:17:46 -0400

    Quick commit

---
+++ Practice/Class/CSC 211/7-19-23/SocSecProcessor.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public interface SocSecProcessor {
+    public static void main(String[] args) {
+        System.out.println("Main method started");
+        boolean continueInput = true;
+        while(continueInput)
+        {
+            try {
+                Scanner keyb = new Scanner(System.in);
+                String name = JOptionPane.showInputDialog("Enter a name:");
+                String ssn = JOptionPane.showInputDialog("Enter a social security number (xxx-xx-xxxx):");
+                if (isValid(ssn)) {
+                    System.out.println(name + " " + ssn + " is valid");
+                }
+            } catch (SosSecException e) {
+                System.out.println(e.getMessage());
+            }
+            String continueChoice = JOptionPane.showInputDialog("Continue? (y/n):");
+            continueInput = continueChoice.equalsIgnoreCase("y");
+        }
+        System.out.println("Main method ended");
+    }
+
+
+    private static boolean isValid(String ssn) throws SosSecException {
+
+        if (ssn.length() != 11) {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid social security number, wrong number of characters",
+                    "Warning",
+                    JOptionPane.WARNING_MESSAGE);
+            throw new SosSecException("Invalid social security number, wrong number of characters");
+        }
+        if (ssn.charAt(3) != '-' || ssn.charAt(6) != '-') {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid social security number, dashes at wrong positions",
+                    "Warning",
+                    JOptionPane.WARNING_MESSAGE);
+            throw new SosSecException("Invalid social security number, dashes at wrong positions");
+        }
+        for (char c : ssn.toCharArray()) {
+            if (!Character.isDigit(c) && c != '-') {
+                JOptionPane.showMessageDialog(null,
+                        "Invalid social security number, contains a character that is not a digit",
+                        "Warning",
+                        JOptionPane.WARNING_MESSAGE);
+                throw new SosSecException("Invalid social security number, contains a character that is not a digit");
+            }
+        }
+        return true;
+    }
+
+}
