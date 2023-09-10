e7fd84550c6ea2b13015d863edbb5e72cd1e40ee
Mike Odnis <airwolf635@gmail.com>
2023-09-10 16:21:27 -0400

    Quick commit

---
+++ Practice/Class/CSC 229/Public-Key-Crypt/MainDriver.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MainDriver {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+  }
+  public static String PublicKey(String message, String key1, String key2) {
+    return "";
+  }
+  public static String PrivateKey(String message, String key1, String key2) {
+    return "";
+  }
+}
