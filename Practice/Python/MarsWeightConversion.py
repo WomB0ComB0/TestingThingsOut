4b5aef09c19799df20317438c3be70c962004874
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2024-03-17 05:25:22 -0400

    Quick commit

--- Practice/Python/MarsWeightConversion.py
+++ Practice/Python/MarsWeightConversion.py
@@ -1,14 +1,18 @@
 class MarsWeightConversion:
+    # __init__ is a reserved method in Python classes. It is known as a constructor in object oriented concepts. This method called when an object is created from the class and it allows the class to initialize the attributes of the class.
     def __init__(self, weight: str) -> None:
         self.weight = weight
-        self.MARS_MULTIPLE = 0.38 # Constant, cannot be changed
+        self.MARS_MULTIPLE = 0.38 # Mars' gravity is 0.38 times that of Earth's
 
     def convert_to_mars_weight(self) -> float:
+        # The __round__() method returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
         return (float(self.weight) * self.MARS_MULTIPLE).__round__(2)
 
     def convert_to_earth_weight(self) -> float:
         return (float(self.weight) / self.MARS_MULTIPLE).__round__(2)
 
+    # Magic methods in Python are special methods that start and end with double underscores. They are also called dunder methods.
+    # A method is called when it is bound to a class or an instance of the class. Magic methods are not called directly, but they are called when certain operations are performed on the object.
     def __str__(self):
         return f"Weight on Mars: {self.convert_to_mars_weight()}"
 
@@ -19,8 +23,9 @@ class Sanitization:
         self.weight = self.includes_science_notation()
 
     def clean_input(self) -> str:
+        # Regular expressions are a powerful tool for various kinds of string manipulation. They are a domain specific language (DSL) that is present as a library in most modern programming languages, not just Python.
         invalid_characters = re.findall(r"[!@#$%^&*()_+=,<>?/;:|\\]", self.user_input)
-
+        
         cleansed = re.sub(r"[^\d.]", "", self.user_input)
 
         if invalid_characters:
@@ -32,6 +37,9 @@ class Sanitization:
         if cleansed:
             return cleansed
         else:
+            # raise an exception if the input is invalid, i.e. if the input is empty
+            # ValueError is a built-in exception in Python that is raised when the built-in functions for a data type have the valid type of arguments, but the arguments have invalid values specified.
+            # Raise is a keyword in Python that is used to raise an exception. It is used to raise an exception explicitly.
             raise ValueError(
                 f"Invalid input. Please enter a valid weight and remove the invalid characters from {self.user_input}"
             )
@@ -107,9 +115,13 @@ class TestMarsWeightConversion:
             print("\n")
 
 def main():
+    # If you're worried about the the return type of the input() function, you can use the following code to check the type of the input:
+    # user_input = input("Enter your weight on Earth: ")
+    # print(type(user_input))
     user_input = input("Enter your weight on Earth: ")
+    # You call the Sanitization class and pass the user_input as an argument to the class. The class then returns the weight attribute.
     weight_conversion = Sanitization(user_input)
-    print(MarsWeightConversion(weight_conversion.weight).convert_to_mars_weight())
+    print(MarsWeightConversion(weight_conversion.weight).__str__())
 
     # test = TestMarsWeightConversion("100")
     # print(test.test_mars_weight_conversion())
