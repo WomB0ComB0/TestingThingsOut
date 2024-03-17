6369094f3ff75cd1aa03ba214f562f6f14c9a6a0
Mike Odnis <airwolf635@gmail.com>
2024-03-17 17:49:07 -0400

    Quick commit

--- MicrosoftLearn/C#/MauiApp1/MauiApp1/obj/Debug/net7.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ MicrosoftLearn/C#/MauiApp1/MauiApp1/obj/Debug/net7.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+92a376cb8774aa8449857118201bb643d82d6cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2e435969b3220401cf55f2f2c709ecce9e0f3ee3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
